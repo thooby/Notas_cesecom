faaa1f9125e04631763f739a7fdee67e3d0177f9
Juan Zubillaga <juan@zubi.net>
2010-07-25 03:26:06 +0200

    Comienzo arreglos notas

--- app/models/curso.rb
+++ app/models/curso.rb
@@ -3,6 +3,7 @@ class Curso < ActiveRecord::Base
   belongs_to :nivel
   has_many :creditos
   has_many :estudiantes, :through => :creditos
+  belongs_to :docente
   def curso_caract
     "<b>"+self.materia.titulo+ "</b> "+self.materia.descripcion + " ("+ self.nivel.descripcion+")"
   end
--- app/models/docente.rb
+++ app/models/docente.rb
@@ -1,4 +1,7 @@
 class Docente < ActiveRecord::Base
   belongs_to :sexo
   has_many :cursos
+  def nombre_completo
+    self.nombre1 + " "+ self.nombre2+" "+self.apellido1+" "+self.apellido2
+  end
 end
