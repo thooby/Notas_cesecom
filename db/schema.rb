dd6d07ee16f7ec3777fd823ad4b3570309c4da93
Juan Zubillaga <juan@zubi.net>
2010-07-28 02:56:42 +0200

    adding nifty_authentication

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100717014913) do
+ActiveRecord::Schema.define(:version => 20100728005538) do
 
   create_table "creditos", :force => true do |t|
     t.integer  "estudiante_id"
@@ -87,6 +87,15 @@ ActiveRecord::Schema.define(:version => 20100717014913) do
 
   create_table "tipo_notas", :force => true do |t|
     t.string   "descripcion"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", :force => true do |t|
+    t.string   "username"
+    t.string   "email"
+    t.string   "password_hash"
+    t.string   "password_salt"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
