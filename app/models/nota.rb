fb01cbf74ee46bb704e805efedb2ed33788dac8f
Juan Zubillaga <juan@zubi.net>
2010-07-28 07:18:50 +0200

    funcionando la autenticacion

--- app/models/nota.rb
+++ app/models/nota.rb
@@ -3,4 +3,5 @@ class Nota < ActiveRecord::Base
   belongs_to :tipo_nota
   accepts_nested_attributes_for :tipo_nota, :reject_if => lambda { |a| a[:content].blank?}, :allow_destroy => true  
   accepts_nested_attributes_for :credito, :reject_if => lambda { |a| a[:content].blank?}, :allow_destroy => true  
+  default_scope :order => '`notas`.credito_id ASC'
 end
